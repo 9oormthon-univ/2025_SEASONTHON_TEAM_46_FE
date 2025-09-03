d3fc1c7a124c3639f936f49e825c6bee54943458
javadocq <han18264@naver.com>
2025-09-03 15:18:41 +0900

    feat: searchbar parameter

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,12 +1,35 @@
 import SearchSvg from "../../assets/icons/search.svg";
 
-export function SearchBar() {
+/**
+ * SearchBar component for searching news articles.
+ * @param searchText - The text to search for.
+ * @param setSearchText - The function to update the search text.
+ * @param onClick - The function to call when the search button is clicked.
+ * @returns {JSX.Element}
+ */
+
+export function SearchBar({
+  searchText,
+  setSearchText,
+  onClick,
+}: {
+  searchText: string;
+  setSearchText: (text: string) => void;
+  onClick: () => void;
+}) {
   return (
     <div className="flex h-[56px] w-[337px] items-center gap-[13px] rounded-[50px] bg-[rgba(255,255,255,0.95)] px-[27px]">
-      <img src={SearchSvg} alt="Search" />
+      <img
+        src={SearchSvg}
+        alt="Search"
+        onClick={onClick}
+        className="cursor-pointer"
+      />
       <input
+        value={searchText}
         type="text"
         placeholder="뉴스를 검색해보세요."
+        onChange={(event) => setSearchText(event.target.value)}
         className="w-full bg-transparent text-[16px] font-[500] text-[rgba(42,42,42)] outline-none placeholder:text-[rgba(42,42,42,0.30)]"
       />
     </div>
--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { SearchBar } from "../common/SearchBar";
 
 export function Header() {
+  const [searchText, setSearchText] = useState<string>("");
   return (
-    <header className="h-[346px] w-[391px] gap-[27px] bg-gradient-to-b from-[#3D57FE] to-[#7F81FF] px-[28px] py-[58px]">
+    <header className="flex h-[346px] w-[391px] flex-col gap-[27px] bg-gradient-to-b from-[#3D57FE] to-[#7F81FF] px-[28px] py-[58px]">
       <h1 className="text-[32px] font-bold text-white">NewsTHINKY</h1>
-      <SearchBar />
+      <SearchBar
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onClick={() => {}}
+      />
     </header>
   );
 }
