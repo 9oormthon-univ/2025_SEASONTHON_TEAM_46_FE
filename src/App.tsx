50ffa8a9243a445a1f55f7607ea91bff7473c351
javadocq <han18264@naver.com>
2025-09-04 12:27:22 +0900

    feat: login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BottomNav } from "./components/common/BottomNav";
+import { useStore } from "./stores/useStore";
 import HomePage from "./pages/HomePage";
 import HotPage from "./pages/HotPage";
 import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
 
 function App() {
+  const showBottomNav: boolean = useStore((state) => state.showBottomNav);
   return (
     <BrowserRouter>
       <div className="relative mx-auto min-h-screen w-[393px]">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/hot" element={<HotPage />} />
           <Route path="/my" element={<MyPage />} />
         </Routes>
-
-        <BottomNav />
+        {showBottomNav && <BottomNav />}
       </div>
     </BrowserRouter>
   );
