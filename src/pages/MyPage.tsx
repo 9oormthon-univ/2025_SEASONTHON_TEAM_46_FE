13cc9cf8f926174cce8720120e0b824270ff6816
javadocq <han18264@naver.com>
2025-09-04 22:14:07 +0900

    feat: activity component

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,11 +1,13 @@
 import CommonHeader from "../components/common/Header";
+import { Activity } from "../components/my/Activity";
 import { Profile } from "../components/my/Profile";
 import { ProfileData } from "../constant/ProfileData";
 export default function MyPage() {
   return (
-    <article className="flex h-screen w-full flex-col bg-[#FAFAFA]">
+    <article className="flex h-screen w-full flex-col items-center bg-[#FAFAFA]">
       <CommonHeader title="마이페이지" />
       <Profile {...ProfileData} />
+      <Activity />
     </article>
   );
 }
