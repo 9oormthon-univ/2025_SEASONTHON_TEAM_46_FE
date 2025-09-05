2ff07bfbd231c1727c34183e5d092cf072607cf1
honi31 <winner020531@naver.com>
2025-09-05 11:53:53 +0900

    feat: 소비분석 요약 컴포넌트 구현

--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -1,9 +1,16 @@
 import NewsSentimentBar from "../components/report/NewsSentimentBar";
+import SummaryBar from "../components/report/SummaryBar";
 
 export default function ReportPage() {
   return (
-    <div>
+    <div className="flex gap-8">
       <NewsSentimentBar />
+      <SummaryBar
+        lines={[
+          "이번 주 경제 기사보다 정치 기사에 더 많은 시간 소비",
+          "부정적 뉴스 비율이 전체의 50%로 높게 나타남",
+        ]}
+      />
     </div>
   );
 }
