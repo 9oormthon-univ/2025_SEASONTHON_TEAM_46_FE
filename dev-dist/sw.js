df12d01b424f0b8cdc0b0b1f345cd451107de105
honi31 <winner020531@naver.com>
2025-09-07 03:41:23 +0900

    fix: 바텀시트 카테고리 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(["./workbox-86c9b217"], function (workbox) {
     [
       {
         url: "index.html",
-        revision: "0.99u4t21ebm8",
+        revision: "0.k5hd1bh2uoo",
       },
     ],
     {},
